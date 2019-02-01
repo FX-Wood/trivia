f1315e6649bff74d42f7cd886dadf4bfec72f36a
FX-Wood <fxwood2@gmail.com>
2019-02-01 11:00:41 -0800

    moved html escape fix higher, to fetch function

--- app.js
+++ app.js
@@ -9,37 +9,7 @@ var labels = document.querySelectorAll('.label');
 var radios = document.querySelectorAll('.radio-selection');
 var nextBtn = document.getElementById('next-btn')
 var yourAnswers = []
-var triviaArr = [ {
-    question: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?",
-    correctAnswer: "A. William and Elizabeth",
-    answers: [
-        "B. Joseph and Catherine",
-        "C. John and Mary",
-        "D. George and Anne"
-    ]
-},
-{
-    question: "When did the Liberty Bell get its name?",
-    correctAnswer: "D. none of the above",
-    answers: [
-        "A. when it was made, in 1701",
-        "B. when it rang on July 4, 1776",
-        "C. in the 19th century, when it became a symbol of the abolition of slavery"
-    ]
-    //rightAnswerIndex: 3
-},
-
-{
-    question: "In the Roy Rogers -Dale Evans Museum, you will find Roy and Dales stuffed horses. Roy's horse was named Trigger, which was Dales horse?",
-    correctAnswer: "A. Buttermilk",
-    answers: [
-        "B. Daisy",
-        "C. Scout",
-        "D. Tulip"
-    ]
-    //rightAnswerIndex: 0
-}
-]
+var triviaArr = []
 //API: 
 fetch(url)
 .then(function(data){
@@ -48,29 +18,21 @@ fetch(url)
 .then(function(data){
     return data.results
 })
-
 .then(function(results){
   triviaArr=[];
   results.forEach(function(result){
     var output = {}
-    output.question = result.question
-    output.correctAnswer = result.correct_answer
-    output.answers = result.incorrect_answers
+    output.question = fixString(result.question)
+    output.correctAnswer = fixString(result.correct_answer)
+    output.answers = result.incorrect_answers.map(fixString)
     triviaArr.push(output)
     console.log(output)
   })
 })
-// start game
 .then(function(results) {
     fillQuestion()
     nextBtn.addEventListener('click', nextButtonFct);
 })
-
-
-
-
-//a question appears
-
 
 function nextButtonFct() {
     var checked;
@@ -86,25 +48,22 @@ function nextButtonFct() {
 }
 
 function fillQuestion() {
-    //if the current question is as long as the triviaArr, then go to score page
     //if the current question is shorter then the TriviaArr, then continue to next question. 
     if (currentQuestion < triviaArr.length){
-        mainStyle.style.backgroundColor= rainbow[currentQuestion];
-        questionBox.textContent = fixString(triviaArr[currentQuestion].question)
+        mainStyle.style.backgroundColor= pickRandom(rainbow);
+        questionBox.textContent = triviaArr[currentQuestion].question
         var currentAnswers = pickRandom()
         labels.forEach(function(label, index) {
             label.textContent = currentAnswers[index]
         })
+    //if the current question is as long as the triviaArr, then go to score page
     }  else if (currentQuestion >= triviaArr.length){
         mainStyle.style.backgroundColor = "dodgerblue"
         triviaArr.forEach(function(question) {
-
         })
-        
         scoreboard.textContent = `${score}`
         yourAnswers.forEach(function(answer) {
             let debrief = document.createElement('div')
-
             debrief.textContent += answer.yourChoice
             debrief.textContent += answer.correctAnswer
             debrief.textContent += answer.question
@@ -113,31 +72,28 @@ function fillQuestion() {
             // } else if (answer.correctAnswer === answer.yourChoice) {
             //     debrief.style.color = "green";
             // }
-            mainStyle.style.padding = ''
-            scoreboard.style.background = 'dodgerblue'
             scoreboard.appendChild(debrief)
         })
-
-
         scoreboard.classList.remove("hide");
     }
-    }
+}
 
-function pickRandom() {
-    // make an array with all the answers
-    var unRandomAnswers = [].concat(triviaArr[currentQuestion].answers)
-    unRandomAnswers.push(triviaArr[currentQuestion].correctAnswer)
-    // answers will be an array with incorrect answers first and the correct one last.
+function pickRandom(arr) {
+    var unRandomAnswers = [];
     var randomAnswers = [];
-    // pick a number between 0 and length of the array -1
+    if (!arr) {
+        unRandomAnswers = [].concat(triviaArr[currentQuestion].answers)
+        unRandomAnswers.push(triviaArr[currentQuestion].correctAnswer)
+    } else (
+        unRandomAnswers = arr.splice()
+    )
     for (i=0; i < 4; i++) {
         var rand = Math.random() // between 0 and 1
         var selection = Math.floor(rand*unRandomAnswers.length-1)
-        randomAnswers.push(fixString(unRandomAnswers.splice(selection, 1)[0]))
+        randomAnswers.push(unRandomAnswers.splice(selection, 1)[0])
     }
     return randomAnswers
 }
-
 function checkAnswer () {
     var theOneTheyChose;
     radios.forEach(function(option) {
@@ -169,7 +125,6 @@ function checkAnswer () {
     }
     currentQuestion++
 }
-
 function fixString(string) {
     return string.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&ouml;/, 'ö').replace(/&amp;/g, '&').replace(/&ntilde;/g, 'ñ')
 }
